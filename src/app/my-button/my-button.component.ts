42b83c1390aa8240b1628c24c28f07ecabca1925
Sterling Espinoza <sterlingske@gmail.com>
2018-09-24 20:30:00 -0700

    second push ster-sprint1

--- src/app/my-button/my-button.component.ts
+++ src/app/my-button/my-button.component.ts
@@ -8,18 +8,23 @@ export class MyButtonComponent implements OnInit {
 
   constructor() { }
 
-  buttonFunction1() {
-    document.getElementById("myParagraph").innerHTML = "You did it!";
-
-  }
   timerFunction1() {
-    document.getElementById("myParagraph").innerHTML = "Or did you?";
+    document.getElementById("myParagraph").innerHTML = "I'm not null...";
 
   }
+  timerFunction2() {
+    document.getElementById("myParagraph").innerHTML = "Or am I?";
+    clearInterval(this.timerVar1);
 
-  timerVar = setInterval(this.timerFunction1, 5000);
-  
-  //buttonVar = document.getElementById("myButton").addEventListener("click", this.buttonFunction1);
+  }  
+  timerFunction3() {
+    document.getElementById("myParagraph").innerHTML = "Really I'm not.";
+    clearInterval(this.timerVar2);
+  }
+
+  timerVar1 = setInterval(this.timerFunction1, 3000);
+  timerVar2 = setInterval(this.timerFunction2, 5000);
+  timerVar3 = setInterval(this.timerFunction3, 7000);
 
   ngOnInit() {
 
